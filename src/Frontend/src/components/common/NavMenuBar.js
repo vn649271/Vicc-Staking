ce2e2484c2fc8fc2c7c8a5d0891adee556d5ba6c
Cowboy <vn649271@gmail.com>
2022-06-06 08:15:48 +0200

    - Implemented responsiveness of menu bar

---
+++ src/Frontend/src/components/common/NavMenuBar.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react'
+import { useHistory } from "react-router-dom";
+import './nav-bar.css'
+
+export default function NavMenuBar() {
+
+	const [selectedMenuItem, setSelectedMenuItem] = useState('home');
+
+	const [toggleMenu, setToggleMenu] = useState(false)
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+	let history = useHistory();
+
+	useEffect(() => {
+	    const changeWidth = () => {
+	      setScreenWidth(window.innerWidth);
+	    }
+	    window.addEventListener('resize', changeWidth)
+	    return () => {
+	        window.removeEventListener('resize', changeWidth)
+	    }
+	}, [])
+	const toggleNav = () => {
+		setToggleMenu(!toggleMenu)
+	}
+	const onSelectMenuItem = (ev) => {
+		let selectedItem = ev.target.id.replace('menu-item-', '');
+		setSelectedMenuItem(selectedItem);
+		history.push("/" + selectedItem);
+	}
+
+	return (
+	    <nav>
+	    	{(toggleMenu || screenWidth > 500) && (
+				<ul className="nav-menu-list">
+					<li id="menu-item-home" className={`items ${selectedMenuItem} == 'home'? 'active': ''`} onClick={onSelectMenuItem}>Home</li>
+					<li id="menu-item-about" className={`items ${selectedMenuItem} == 'about'? 'active': ''`} onClick={onSelectMenuItem}>About</li>
+					<li id="menu-item-tokenomics" className={`items ${selectedMenuItem} == 'tokenomics'? 'active': ''`} onClick={onSelectMenuItem}>Tokenomics</li>
+					<li id="menu-item-roadmap" className={`items ${selectedMenuItem} == 'roadmap'? 'active': ''`} onClick={onSelectMenuItem}>Roadmap</li>
+					<li id="menu-item-contactus" className={`items ${selectedMenuItem} == 'contactus'? 'active': ''`} onClick={onSelectMenuItem}>Contact us</li>
+			    </ul>
+			)}
+			<button onClick={toggleNav} className="btn">BTN</button>
+	    </nav>
+	)
+}
