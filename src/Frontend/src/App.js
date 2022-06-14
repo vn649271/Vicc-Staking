cbcc3ecb15f1375b1bdbfedbd3dd428a7ebcf9fc
Cowboy <vn649271@gmail.com>
2022-06-14 16:50:40 +0200

    + LandingPage

--- src/Frontend/src/App.js
+++ src/Frontend/src/App.js
@@ -4,28 +4,29 @@ import HomePage from "./pages/HomePage";
 import StakingPage from "./pages/StakingPage";
 import RoadmapPage from "./pages/RoadmapPage";
 import TokenomicsPage from "./pages/TokenomicsPage";
+import LandingPage from "./pages/landingpage/LandingPage";
 
 const App = () => {
   return (
     <>
-    <Switch>
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
-      <Route path="/home" exact>
-        <HomePage />
-      </Route>
-      <Route path="/stake" exact>
-        <StakingPage />
-      </Route>
-      <Route path="/roadmap" exact>
-        <RoadmapPage />
-      </Route>
-      <Route path="/tokenomics" exact>
-        <TokenomicsPage />
-      </Route>
-    </Switch>
-  </>
+      <Switch>
+        <Route path="/" exact>
+          <LandingPage />
+        </Route>
+        <Route path="/home" exact>
+          <HomePage />
+        </Route>
+        <Route path="/stake" exact>
+          <StakingPage />
+        </Route>
+        <Route path="/roadmap" exact>
+          <RoadmapPage />
+        </Route>
+        <Route path="/tokenomics" exact>
+          <TokenomicsPage />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
