87e2a28c39de497d646f36a4c8c5ddee16b13cad
Cowboy <vn649271@gmail.com>
2022-06-15 05:03:29 +0200

    - Constructed build environment on Ganache

--- src/Contract/truffle-config.js
+++ src/Contract/truffle-config.js
@@ -22,6 +22,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic_ganache = fs.readFileSync(".secret.ganache").toString().trim();
 const BSC_API_KEY = fs.readFileSync("apikey").toString().trim();
 
 module.exports = {
@@ -43,9 +44,9 @@ module.exports = {
     // options below to some value.
     //
     ganache: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "5777",       // Any network (default: none)
+      provider: () => new HDWalletProvider(mnemonic_ganache, `http://127.0.0.1:7545`),
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: 5777,
     },
     bsctest: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
@@ -61,34 +62,6 @@ module.exports = {
       timeoutBlocks: 200,
       skipDryRun: true
     },    
-    //
-    // An additional network, but with some advanced options…
-    // advanced: {
-    //   port: 8777,             // Custom port
-    //   network_id: 1342,       // Custom network
-    //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    //   from: <address>,        // Account to send transactions from (default: accounts[0])
-    //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
-    // },
-    //
-    // Useful for deploying to a public network.
-    // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // ropsten: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    //   network_id: 3,       // Ropsten's id
-    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    //
-    // Useful for private networks
-    // private: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    //   network_id: 2111,   // This network is yours, in the cloud.
-    //   production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
   // Set default mocha options here, use special reporters, etc.
