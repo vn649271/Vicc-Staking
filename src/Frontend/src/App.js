54058f515897cf6c04c892fb840f35e3639c4e2d
Cowboy <vn649271@gmail.com>
2022-06-15 02:18:56 +0200

    - Refined

--- src/Frontend/src/App.js
+++ src/Frontend/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import StakingPage from "./pages/StakingPage";
-import RoadmapPage from "./pages/RoadmapPage";
-import TokenomicsPage from "./pages/TokenomicsPage";
 import LandingPage from "./pages/landingpage/LandingPage";
 
 const App = () => {
@@ -13,17 +10,8 @@ const App = () => {
         <Route path="/" exact>
           <LandingPage />
         </Route>
-        <Route path="/home" exact>
-          <HomePage />
-        </Route>
         <Route path="/staking" exact>
           <StakingPage />
-        </Route>
-        <Route path="/roadmap" exact>
-          <RoadmapPage />
-        </Route>
-        <Route path="/tokenomics" exact>
-          <TokenomicsPage />
         </Route>
         <Route path="/token_contract" exact>
           <a href="https://testnet.bscscan.com/token/0x2b3d6dfd444695e9aa37c0e4e8fc47f26c9cc594" />
