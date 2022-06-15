6c4c76614a020ddce3c5d5e9368f2e286907eb01
vn649271 <vn649271@gmail.com>
2022-06-16 01:13:48 +0200

    + Use 2nd rpc url from   "https://docs.bscscan.com/misc-tools-and-utilities/public-rpc-nodes#testnet-bsc-rpc-nodes-chainid-97https://docs.bscscan.com/misc-tools-and-utilities/public-rpc-nodes#testnet-bsc-rpc-nodes-chainid-97" to deploy contract

--- src/Contract/truffle-config.js
+++ src/Contract/truffle-config.js
@@ -48,7 +48,7 @@ module.exports = {
       network_id: 5777,
     },
     bsctest: {
-      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-2-s1.binance.org:8545`),
       network_id: 97,
       confirmations: 10,
       timeoutBlocks: 200,
