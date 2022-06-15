27e2f64dc0947b965742a611073a1561a3dd413c
Cowboy <vn649271@gmail.com>
2022-06-15 02:39:19 +0200

    +

--- src/Frontend/src/utils/constants.js
+++ src/Frontend/src/utils/constants.js
@@ -1,12 +1,12 @@
 // import stakingABI from "../abis/Staking.json";
 // import VictoryCashABI from "../abis/BC.json";
-import StakingDeployedInfo from "../abis/VictoryCoinStaking.json";
+import ViccStakingDeployedInfo from "../abis/Vicctaking.json";
 import ViccDeployedInfo from "../abis/ViccToken.json";
 
 export const CHAIN_ID = 97;
 export const stakingDeployedInfo = {
-	abi: StakingDeployedInfo.abi,
-	address: StakingDeployedInfo.networks[CHAIN_ID].address
+	abi: ViccStakingDeployedInfo.abi,
+	address: ViccStakingDeployedInfo.networks[CHAIN_ID].address
 };
 // export const stakingContractAddress = "0x1808Ec091DB6efa4a06Fd8ADa1C6435eB2a5aEAE";
 export const viccDeployedInfo = {
@@ -14,4 +14,3 @@ export const viccDeployedInfo = {
 	address: ViccDeployedInfo.networks[CHAIN_ID].address
 };
 // export const bcTokenContractAddress = "0x09CD2D1351DeBe79E6da00Fd5d078b4FCe89721C";
-
