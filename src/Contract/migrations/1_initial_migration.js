b2278eca8e71f846e55c141adce00a831221b2f3
vn649271 <vn649271@gmail.com>
2022-06-17 23:13:39 +0200

    -

--- src/Contract/migrations/1_initial_migration.js
+++ src/Contract/migrations/1_initial_migration.js
@@ -7,5 +7,5 @@ module.exports = async function (deployer) {
   console.log(viccDeployed.address);
   viccDeployed.addAdmin("0xADB366C070DFB857DC63ebF797EFE615B0567C1B");
   let viccStakingDeployed = await deployer.deploy(ViccStaking, viccDeployed.address);
-  viccDeployed.addAdmin(viccStakingDeployed.address);
+  await viccDeployed.addAdmin(viccStakingDeployed.address);
 };
